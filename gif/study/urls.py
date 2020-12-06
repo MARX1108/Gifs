488457d93d08ab2a780cf2a0471b9067b3497e81
Marx <boyuan@vt.edu>
2020-12-06 11:38:08 -0500

    session var

--- gif/study/urls.py
+++ gif/study/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.main_render, name='index'),
+    # url(r'^start', views.start, name='start'),
 ]
--- gif/study/views.py
+++ gif/study/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+def main_render(request):
+    if not "step" in request.session:
+        return setup(request)
+    else: 
+        return index(request)
+
 def index(request):
-    var = (request.POST.get('step') == 1)
+    request.session['step'] = request.session['step']+1
     return render(request, 'index.html', {
         'case_count':request.POST.get('step'),
         'sample_id':'08'
     })
 
+def setup(request):
+    request.session['step'] = 0
+    return render(request, 'start.html', {
+        # 'case_count':request.POST.get('step'),
+        # 'sample_id':'08'
+    })
