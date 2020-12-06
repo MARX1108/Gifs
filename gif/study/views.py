f1f24bc858371cec1e570d60c002b7ecd0e960e2
Marx Wang <marxw@Marxs-MacBook-Pro.local>
2020-12-06 11:07:58 -0500

    templates

--- gif/study/views.py
+++ gif/study/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    var = (request.POST.get('step') == 1)
+    return render(request, 'index.html', {
+        'case_count':request.POST.get('step'),
+        'sample_id':'08'
+    })
 
