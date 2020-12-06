0e33d5840d770ee0bbdc7fd7b808d5570d21fb10
Marx <boyuan@vt.edu>
2020-12-06 11:43:03 -0500

    end point

--- gif/study/views.py
+++ gif/study/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def main_render(request):
     if not "step" in request.session:
         return setup(request)
-    else: 
+    elif request.session['step'] >= 10:
+        return end(request)
+    else:
         return index(request)
+
 
 def index(request):
     request.session['step'] = request.session['step']+1
     return render(request, 'index.html', {
-        'case_count':request.POST.get('step'),
-        'sample_id':'08'
+        'case_count': request.POST.get('step'),
+        'sample_id': '08'
     })
+
 
 def setup(request):
     request.session['step'] = 0
     return render(request, 'start.html', {
+        # 'case_count':request.POST.get('step'),
+        # 'sample_id':'08'
+    })
+
+
+def end(request):
+    return render(request, 'end.html', {
         # 'case_count':request.POST.get('step'),
         # 'sample_id':'08'
     })
