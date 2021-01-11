cba8c458824778db6c5c018e72ed2179d349fb45
Marx <boyuan@vt.edu>
2021-01-11 10:08:52 -0500

    db init

--- gif/gif/settings.py
+++ gif/gif/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'gif.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gif',
+
+        'USER': 'admin',
+
+        'PASSWORD': 'gif',
+        'HOST': 'localhost',
+        'PORT': ''
+
     }
 }
 
--- gif/study/models.py
+++ gif/study/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+class Study(models.Model):
+    searchKey = ArrayField(models.CharField(max_length=300))
+    q1 = models.PositiveSmallIntegerField()
+    q2 = models.PositiveSmallIntegerField()
+    q3 = models.PositiveSmallIntegerField()
+    q4 = models.PositiveSmallIntegerField()
+    q5 = models.PositiveSmallIntegerField()
